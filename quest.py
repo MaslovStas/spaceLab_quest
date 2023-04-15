62fd4177ef4ae9be504337d8f7600c03af4882f3
Stasyan <stasyand330@gmail.com>
2023-04-15 13:15:56 +0300

    edit_previous_fields

--- quest.py
+++ quest.py
@@ -8,6 +8,12 @@ class Field:
     """Стандартное поле"""
 
     def __init__(self, board, position: tuple, is_white=True, is_available=True):
+        """
+        :param board: Ссылка на доску, которой принадлежит поле;
+        :param position: Координаты доски, где располагается поле;
+        :param is_white: Является ли поле белым, влияет на запись предыдущего поля и зажжение поля;
+        :param is_available: Является ли поле доступным для перехода.
+        """
         self.board = board
         self.position = position
         self.is_white = is_white
@@ -17,10 +23,11 @@ class Field:
         self.players = []
         self._has_key = False
 
-        self.board[position] = self  # добавляем поле в общую матрицу
+        self.board[position] = self
 
     @property
     def has_key(self):
+        """Лежит ли на поле ключ"""
         return self._has_key
 
     @has_key.setter
@@ -39,17 +46,20 @@ class Field:
         return '0'
 
     def effect_on_player(self, player):
-        """Эффект поля на игрока в зависимости от того горит ли поле"""
+        """Эффект поля на игрока в зависимости от того горит ли поле, в дальнейшем будет суммироваться с эффектами
+        других полей"""
         if self.is_fire:
             player.hurt()
             print(f'{player} получает урон от огня!')
 
     def player_enters(self, player):
-        # если текущая позиция игрока уже не пустая, то это движение игрока, а не инициализация его на карте
+        """Выполняется при входе игрока на поле. Если текущая позиция игрока уже не пустая, то это движение игрока,
+        а не инициализация его на карте. При этом кроме добавления игрока в список игроков этого поля происходит
+        очищение поля, с которого игрок уходит и воздействие эффектов поля на игрока"""
         if player.current_field:
             self.status_field(player)
-            player.current_field.player_leaves(player)  # очищаем клетку, с которой игрок уходит
-            self.effect_on_player(player)  # воздействуем на игрока
+            player.current_field.player_leaves(player)
+            self.effect_on_player(player)
 
         self.players.append(player)
 
@@ -92,7 +102,7 @@ class Altar(Field):
 
 
 class Golem(Field):
-    """Поле с големом и ключом, который и может открыть именно этого голема"""
+    """Поле с големом, является финишным полем. При наличии ключа игрок побеждает, иначе погибает"""
 
     def effect_on_player(self, player):
         super().effect_on_player(player)
@@ -108,7 +118,7 @@ class Golem(Field):
 
 
 class EmptySpace(Field):
-    """Пустое пространство для придания карте прямоугольной формы, также в контексте данного проекта им придается
+    """Пустое поле для придания карте прямоугольной формы, также в контексте данного проекта ему придается
     оранжевый цвет для фильтрации белых полей, на которых может загореться огонь"""
 
     def __init__(self, board, position):
@@ -122,33 +132,44 @@ class Board:
     SYMBOLS = {'a': Altar, 'f': Field, 'w': Wall, 'g': Golem, 'e': EmptySpace}
 
     def __init__(self, original_board, orange_fields, key_field, burning_fields=None):
+        """
+        При создании карты происходит ее рендирования из заданного текстового формата в двухмерный массив объектов
+        полей в зависимости от буквы на оригинальной доске;
+        :param original_board: Двухмерный массив оригинальной доски в текстовом формате;
+        :param orange_fields: Список полей оранжевого цвета;
+        :param key_field: Позиция расположение ключа на поле. При загрузке игры может быть None, так как ключ может
+        принадлежать игроку;
+        :param burning_fields: Список уже горящих полей, может передаваться только при загрузке игры.
+        """
         self.key_location = None
         self.burning_fields = []
 
         self.create_board(original_board, orange_fields)
 
-        # Добавление ключа к заданному полю
         if key_field:
             self.board[key_field].has_key = True
-        # Зажжение полей, если переданы при загрузке ранее сохраненной игры
+
         if burning_fields:
-            self.burning_fields = burning_fields
-            for pos in self.burning_fields:
-                self.board[pos].is_fire = True
+            for pos in burning_fields:
+                field = self.board[pos]
+                field.is_fire = True
+                self.burning_fields.append(field)
+            print('Горят следующие поля: ' + ', '.join(str(field.position) for field in self.burning_fields) + '.')
 
     def create_board(self, original_board, orange_fields):
+        """Конвертация текстовой карты в карту с объектами полей"""
         width, height = len(original_board[0]), len(original_board)
         self.board = np.empty(shape=(height, width), dtype=object)
-        # Конвертация текстовой карты в карту с объектами полей
+
         for i in range(height):
             for j in range(width):
                 self.SYMBOLS[original_board[i][j]](self, position=(i, j))
-        # Оранжевые поля
+
         for pos_x, pos_y in orange_fields:
             self.board[pos_x, pos_y].is_white = False
 
     def burn(self):
-        """В конце раунда случайно из белых полей загораются 4 поля"""
+        """В конце раунда случайно только из белых полей загораются 4 поля"""
         white_fields = [field for field in self.board.flatten() if field.is_white]
         self.burning_fields = np.random.choice(white_fields, size=4, replace=False)
         for field in white_fields:
@@ -165,23 +186,35 @@ class Board:
         return str(self.board)
 
     @property
-    def to_json(self):
+    def to_json(self) -> dict:
+        """Создание словаря для записи в json"""
         return dict(burning_fields=[field.position for field in self.burning_fields],
                     key_location=self.key_location)
 
 
 class Player:
-    MAX_HEALTH = 5  # Максимальное значение здоровья игрока
-    NUMBER_OF_MEDICINE = 3  # Стартовое значение аптечек
+    MAX_HEALTH = 5
+    NUMBER_OF_MEDICINE = 3
 
-    def __init__(self, name, current_field, previous_field=None, health=MAX_HEALTH, number_of_actions=1,
+    def __init__(self, name, current_field, previous_fields=None, health=MAX_HEALTH, number_of_actions=1,
                  number_of_medicine=NUMBER_OF_MEDICINE, has_key=False):
+        """
+        При создании игрока вводятся только первые два параметра, при загрузке - все.
+        :param name: Имя игрока;
+        :param current_field: Текущее поле карты, с расположением игрока.
+        :param previous_fields: Список предыдущих мест расположений игрока на карте, может быть только одно белое и
+        одно оранжевое.
+        :param health: Текущее здоровье игрока.
+        :param number_of_actions: Количество действий еще доступных игроку, обновляется в конце раунда.
+        :param number_of_medicine: Текущее количество аптечек игрока.
+        :param has_key: Есть ли ключ у игрока, необходимо наличие для победы.
+        """
         self.name = name
         self.health = health
-        self.previous_field = previous_field
+        self.previous_fields = previous_fields if previous_fields else []
         self._current_field = None
         self.current_field = current_field
-        self.number_of_actions = number_of_actions  # Количество действий для одного хода
+        self.number_of_actions = number_of_actions
         self.number_of_medicine = number_of_medicine
         self.has_key = has_key
         self.is_win = False
@@ -195,31 +228,41 @@ class Player:
 
     @current_field.setter
     def current_field(self, field):
-        field.player_enters(self)
-
-        if field == self.previous_field:
+        """При изменении текущей локации игрока проверяется не является ли ход игрока бегством, а также является ли
+        поле доступным для перехода (field.is_available), а также менять значение предыдущего поля, если это именно
+        движение игрока и новое поле белого цвета"""
+        if field in self.previous_fields:
             self.die()
             print(f'{self} струсил и убежал!')
+            return
 
-        if field.is_available:  # изменять локацию игрока, если в клетку разрешен вход
-            # проверяем это инициализация или передвижение игрока и какого цвета новая клетка
+        field.player_enters(self)
+
+        if field.is_available:
             if self.current_field and field.is_white:
-                self.previous_field = self.current_field
+                if self.current_field.is_white:
+                    self.previous_fields = [self.current_field]
+                else:
+                    self.previous_fields.append(self.current_field)
 
             self._current_field = field
 
     def spend_action(self, value=1):
+        """Трата действий игрока при совершении хода"""
         self.number_of_actions -= value
 
     def move(self, field):
+        """Движение игрока"""
         self.current_field = field
         self.spend_action()
 
     def strike(self, other):
+        """Удар игрока"""
         print(f'{self} ударил Героя {other.name}')
         other.hurt()
 
     def strike_all_in_the_field(self):
+        """Удар игрока по всем игрокам, если они присутствуют на поле, иначе действие не тратится"""
         if len(self.current_field.players) > 1:  # если в текущем поле, есть другие игроки кроме себя
             for player in self.current_field.players:
                 if player != self:
@@ -230,9 +273,11 @@ class Player:
             print('А ударить-то некого!')
 
     def hurt(self, damage=1):
+        """Ранение игрока"""
         self.health -= damage
 
     def take_key(self):
+        """Взять ключ, если он области досягаемости"""
         if self.current_field.has_key:
             self.has_key = True
             self.current_field.has_key = False
@@ -243,11 +288,13 @@ class Player:
             print('А брать-то нечего!')
 
     def drop_key(self):
+        """Сброс ключа в текущее поле"""
         if self.has_key:
             self.has_key = False
             self.current_field.has_key = True
 
     def heal(self):
+        """Лечение игрока"""
         if self.number_of_medicine > 0:
             self.number_of_medicine -= 1
             self.health = min(self.health + 1, self.MAX_HEALTH)
@@ -262,13 +309,20 @@ class Player:
         self.health = self.MAX_HEALTH
 
     def die(self):
+        """Смерть игрока"""
         self.health = 0
 
     @property
-    def to_json(self):
+    def is_alive(self):
+        return self.health > 0
+
+    @property
+    def to_json(self) -> dict:
+        """Создание словаря для записи в json"""
         return dict(name=self.name,
                     health=self.health,
-                    previous_field=self.previous_field.position if self.previous_field else None,
+                    previous_fields=[field.position
+                                     for field in self.previous_fields] if self.previous_fields else None,
                     current_field=self.current_field.position,
                     number_of_actions=self.number_of_actions,
                     number_of_medicine=self.number_of_medicine,
@@ -283,9 +337,9 @@ class Game:
                       ['w', 'f', 'f', 'w', 'f', 'f', 'f', 'w', 'e', 'e'],
                       ['e', 'w', 'w', 'e', 'w', 'w', 'w', 'e', 'e', 'e']]
 
-    ORANGE_FIELDS = ((2, 3), (1, 5), (3, 7))  # Оранжевые поля
-    START_FIELD = (4, 1)  # Стартовая позиция игроков
-    KEY_FIELD = (2, 3)  # Поле с ключом
+    ORANGE_FIELDS = ((2, 3), (1, 5), (3, 7))
+    START_FIELD = (4, 1)
+    KEY_FIELD = (2, 3)
 
     def __init__(self):
         self.board = Board(self.ORIGINAL_BOARD, self.ORANGE_FIELDS, self.KEY_FIELD)
@@ -309,6 +363,8 @@ class Game:
         print('-' * 100)
 
     def creating_game(self):
+        """При наличии файла сохраненной игры и успешной попытки декодирования данных, игроку предлагается загрузить
+        игру. При отказе создается новая игра. Также по-умолчанию файл сохраненной игры всегда обнуляется."""
         try:
             with open('save.json') as f:
                 save = json.load(f)
@@ -324,10 +380,12 @@ class Game:
             open('save.json', 'w').close()
 
     def new_game(self):
+        """Новая игра"""
         self.board = Board(self.ORIGINAL_BOARD, self.ORANGE_FIELDS, self.KEY_FIELD)
         self.creating_players()
 
     def load_game(self, save):
+        """Загрузка игра"""
         print('Загрузка карты...')
         key_field = tuple(save['board']['key_location']) if save['board']['key_location'] else None
         burning_fields = [tuple(pos) for pos in save['board']['burning_fields']]
@@ -335,11 +393,14 @@ class Game:
         print('Загрузка игроков...')
         for player in save['players']:
             current_field = self.board[tuple(player['current_field'])]
-            previous_field = self.board[tuple(player['previous_field'])] if player['previous_field'] else None
+            if player['previous_fields']:
+                previous_fields = [self.board[tuple(pos)] for pos in player['previous_fields']]
+            else:
+                previous_fields = None
 
             player_load = Player(player['name'],
                                  current_field=current_field,
-                                 previous_field=previous_field,
+                                 previous_fields=previous_fields,
                                  health=player['health'],
                                  number_of_actions=player['number_of_actions'],
                                  number_of_medicine=player['number_of_medicine'],
@@ -347,7 +408,7 @@ class Game:
             self.players.append(player_load)
 
     def creating_players(self):
-        """Создание игроков, размещение их на стартовой позиции поля и добавление в общий список"""
+        """Создание игроков, размещение их на стартовой позиции поля и добавление в список"""
         while True:
             answer = input('Введите количество игроков: ')
             if not answer.isdigit():
@@ -358,32 +419,35 @@ class Game:
                 player = Player(name, current_field=self.board[self.START_FIELD])
                 self.players.append(player)
 
-            break
+            return
 
     def turn(self, player):
-        while player.number_of_actions:  # Пока игрок не потратит действия
-            self.check_health(player)  # проверка жизней до хода
+        """Ход игрока. В начале и в конце хода проверяется количество здоровья игрока. Пока игрок не совершит
+        результативное действие у него будет возможность ходить. Также при возможности взять ключ, ему будет выведена в
+        сообщение"""
+        while player.number_of_actions and player.is_alive:
 
             if player.current_field.has_key:
                 print(f'{player} может поднять ключ!')
 
             move_directions = {'вверх': (-1, 0), 'вниз': (1, 0), 'влево': (0, -1), 'вправо': (0, 1)}
-            actions_player = {'ударить': player.strike_all_in_the_field,
-                              'взять': player.take_key,
-                              'лечить': player.heal,
-                              'сохранить': self.save}
+            actions = {'ударить': player.strike_all_in_the_field,
+                       'взять': player.take_key,
+                       'лечить': player.heal,
+                       'сохранить': self.save}
             action = input(f'{player.name}, Ваш ход: ').lower()
-            if action in move_directions.keys():
+            if action in move_directions:
                 dx, dy = move_directions[action]
                 pos_x, pos_y = player.current_field.position
                 new_field = self.board[pos_x + dx, pos_y + dy]
                 player.move(new_field)
-            elif action in actions_player.keys():
-                actions_player[action]()
+            elif action in actions:
+                actions[action]()
             else:
                 print('Не понял Ваши намерения...')
 
-            self.check_health(player)  # проверка в конце хода
+        if not player.is_alive:
+            self.death_player(player)
 
     def death_player(self, player):
         """При смерти происходит сброс ключа, если он был и освобождение клетки"""
@@ -392,19 +456,16 @@ class Game:
         self.players.remove(player)
         print(f'Герой {player.name} погибает и выбывает из игры...')
 
-    def check_health(self, player):
-        if player.health <= 0:
-            self.death_player(player)
-
     def end_of_round(self):
         """Функция вызывается в конце раунда для восстановления возможности ходить игрокам и зажжению огня на 4-х
         случайных полях"""
         for player in self.players:
             player.number_of_actions = 1
-        # загораются 4 случайных поля
+
         self.board.burn()
 
     def save(self):
+        """Сохранение игры в json-файл"""
         print('Сохранение...')
         with open('save.json', 'w') as f:
             json_obj = dict(players=[player.to_json for player in self.players],
@@ -414,6 +475,8 @@ class Game:
         exit()
 
     def loop(self):
+        """Основной цикл программы. Итерация идет по копии списка игроков, чтобы при удалении игроков из списка итератор
+         не сбивался"""
         while self.players:
             print('-' * 20)
             for player in self.players[:]:
